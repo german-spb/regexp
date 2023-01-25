2bc88616ab071710b8f4e78014b5c76e9ad6e663
Герман <108299345+german-spb@users.noreply.github.com>
2023-01-25 12:25:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ pprint(contacts_list)
 
 def address_book():
     for i in range(len(contacts_list)):
-        values = contacts_list[i][0].split()
-        if len(values) == 3:
-            contacts_list[i][0] = values[0]
-            contacts_list[i][1] = values[1]
-            contacts_list[i][2] = values[2]
-        elif len(values) == 2:
-            contacts_list[i][0] = values[0]
-            contacts_list[i][1] = values[1]
-        elif len(values) == 1:
-            values_2 = contacts_list[i][1].split()
-            if len(values_2) == 2:
-                contacts_list[i][1] = values_2[0]
-                contacts_list[i][2] = values_2[1]
+        words = contacts_list[i][0].split()
+        if len(words) == 3:
+            contacts_list[i][0] = words[0]
+            contacts_list[i][1] = words[1]
+            contacts_list[i][2] = words[2]
+        elif len(words) == 2:
+            contacts_list[i][0] = words[0]
+            contacts_list[i][1] = words[1]
+        elif len(words) == 1:
+            words2 = contacts_list[i][1].split()
+            if len(words2) == 2:
+                contacts_list[i][1] = words2[0]
+                contacts_list[i][2] = words2[1]
 
     pattern = r'(\+7|7|8)?\s?\(?(\d{3})\)?[\s-]?(\d{3})-?(\d{2})-?(\d{2})(\s?\(?доб\.\s?(\d{4})\)?)?'
     new_list = []
@@ -32,6 +32,7 @@ def address_book():
     repl = r'+7(\2)\3-\4-\5\6'
     new_text = re.sub(pattern, repl, text, flags=0)
     new_list2 = new_text.split(',')
+    del new_list2[35]
     my_list = []
     for i in range(len(contacts_list)):
         my_list.append(new_list2[0:7])
@@ -51,9 +52,10 @@ def address_book():
             number = len(updated_contacts_list) - 1
             contacts_dict[name_tuple] = number
 
-    with open("phonebook.csv", "w", encoding='utf-8') as f:
-        datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows(updated_contacts_list)
+        with open("phonebook.csv", "w", encoding='utf-8') as f:
+            datawriter = csv.writer(f, delimiter=',')
+            datawriter.writerows(updated_contacts_list)
+
 
 if __name__ == '__main__':
     address_book()
